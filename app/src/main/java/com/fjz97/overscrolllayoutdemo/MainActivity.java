48f18a43bd9e4cd73f75ac77fd563edf27f7c7e1
fengjingzhe <fengjingzhe@qiyi.com>
2021-02-09 16:08:54 +0800

    迁移AndroidX

--- app/src/main/java/com/fjz97/overscrolllayoutdemo/MainActivity.java
+++ app/src/main/java/com/fjz97/overscrolllayoutdemo/MainActivity.java
@@ -1,10 +1,10 @@
 package com.fjz97.overscrolllayoutdemo;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -51,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getItemCount() {
-            return 10;
+            return 5;
         }
 
         class MovieHolder extends RecyclerView.ViewHolder {
